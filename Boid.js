489b8ee4ee0716ad060b804b225f1910f7883274
Tim Hillier <Tim.r.hillier@gmail.com>
2019-07-04 21:30:07 -0700

    Initial Commit

---
+++ Boid.js
@@ -0,0 +1,171 @@
+function Boid(idNumber = -1,leader = false,speed = 1,size = 1,colour = 'green',x = random(0,width),y=random(0,height))
+{
+    this.level = 1
+    this.speed = speed                  //speed of the boid
+    this.size = size,                   //size of the boid
+    this.leader = leader,               //Something to do with the leader...?????
+    this.colour = colour,               //Colour of the Boid
+    this.idNumber = idNumber            //Id Number of the boid.
+    this.maxLife = 100                  //starting maxLife
+    this.life = 100                     //current life of Biod
+    this.moves = 0                      //how far it has moved in this cycle
+    this.stamina = random(0,3)              //How much stamina the creature has
+    this.experiance = 0                 //the current experiance of the Biod
+    this.nextLevel = 100                //Experiance to the next level
+    this.min = 0                        //Current minimum of level ups for the Biod, will increase with level
+    this.mass = 1
+    this.force = .1
+    this.acceleration = createVector(0,0)
+    this.position = createVector(x,y)
+    this.velocity = createVector(0,0)
+    this.max_acceleration = createVector(.1,.1)
+    this.max_velocity = createVector(.1,.1)
+    // this.
+    
+
+
+ //apply force to the boid
+ //takes a force vector
+ this.applyForce = function(force,r1=0,r2=0,r3=0)
+ {
+    f = p5.Vector.div(force,this.mass)
+    this.acceleration.add(f,r1)
+    this.acceleration.add(r2,r3)
+    this.checkSpeed()
+ }
+
+ //changes in the object physics
+ //is this where i want the boid rules to change the information? I think so
+this.update = function()
+{
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
+    this.checkSpeed()
+    this.acceleration.mult(0)
+}
+
+this.checkSpeed = function()
+{
+    if(this.velocity > this.max_velocity)
+    {
+        this.velocity = this.max_velocity 
+    }
+    
+    if(this.acceleration > this.max_acceleration)
+    {
+        this.acceleration = this.max_acceleration
+    }
+}
+ //life is pain
+ this.live = function()
+ {
+    this.life = this.life - 1
+    this.displayHealth()    
+ }
+ //sometimes good things happen
+this.health = function(heal)
+{
+    this.life = this.life + heal
+    this.displayHealth()
+}
+
+//visually display the units health
+this.displayHealth = function()
+{
+    if(this.life >= (.8 * this.maxLife))
+    {
+        this.colour = 'green'
+        // console.log("Green")
+    }
+    else if((this.life < (.8 * this.maxLife)) && (this.life > (.4 * this.maxLife)))
+    {
+        this.colour = 'yellow'
+        // console.log("Yellow")
+    }
+    else if(this.life <=(.4 * this.maxLife))
+    {
+        this.colour = 'red'
+        // console.log("Red")
+    }
+    else
+    {
+        this.colour = 'blue'
+        console.error("I'm blue da ba dee da ba daa")
+        //should never be here
+    }
+}
+
+
+
+ //checks to see if the spawn is correct. Probably dont need this.
+ this.checkSpawn = function()
+ {
+    if(this.posX > width)//|| realX + X < 0)
+    {
+       this.posX = 0
+    }
+    else if(this.posY < 0)// || realY + Y > windowHeight)
+    {
+        this.posY = height
+    }
+    else if(this.posY > height)
+    {
+        this.posY = 0
+    }
+    else if(this.posX < 0)
+    {
+        this.posX = width
+    }
+ }
+
+//Levels up the Boid...idk
+this.levelUp = function(max)
+{
+    this.level ++
+    if(this.level % 2 == 0)
+    {
+        this.min++
+    }
+    //things to level
+    this.stamina = random(this.min,max)
+}
+
+
+//display the boid on the screen
+this.display = function()
+{
+    //update health
+    this.displayHealth()
+    this.checkPos()
+    stroke(0)
+    strokeWeight(2)
+    fill(this.colour)
+    ellipse(this.position.x,this.position.y,this.mass* 16, this.mass * 16)
+}
+
+//check the position to see if it is off screen
+this.checkPos = function()
+{
+
+    if(this.position.x > width)
+    {
+        this.position.x = 0
+    }
+    if(this.position.x < 0)
+    {
+        this.position.x = width
+    }
+    if(this.position.y > height)
+    {
+        this.position.y = 0
+    }
+    if(this.position.y < 0)
+    {
+        this.position.y = height
+    }
+
+
+}
+
+
+}
---
+++ index.js
@@ -0,0 +1,129 @@
+//P5js environment
+//setup the canvas
+function setup()
+{
+    createCanvas(800,900)
+    // frameRate(30)
+    // background('red')
+
+
+    BOIDS = createBoids(5)
+
+
+}
+
+//draw the boids
+function draw()
+{
+    // moveBoids(BOIDS)
+    background('black')
+    velocityMovement(BOIDS)
+    // background('black')
+    // drawBoids(BOIDS)
+    // console.log(BOIDS.length)
+}
+
+//creates and fills an array with boid objects
+function createBoids(numberOfBoids)
+{
+    BoidArray = []
+    for(i = 0; i < numberOfBoids;i++)
+    {
+        newBoid = new Boid(i)
+        BoidArray.push(newBoid)
+    }
+
+    return BoidArray
+}
+
+//draw the boids on the canvas
+function drawBoids(arrayOfBoids)
+{
+    for(i=0;i<arrayOfBoids.length;i++)
+    {
+        fill(arrayOfBoids[i].colour)
+        // checkPos(arrayOfBoids[i].posX,arrayOfBoids[i].posY)
+        arrayOfBoids[i].checkSpawn()
+        circle(arrayOfBoids[i].posX,arrayOfBoids[i].posY,20)
+    }
+}
+
+//generates a random number
+function randomMovement(biodSpeed = .01)
+{
+    maxMovement = biodSpeed //* 2
+    minMovement = biodSpeed * -1// 3
+    return random(minMovement,maxMovement) 
+}
+
+//make the boid move with velocity
+function velocityMovement(arrayOfBoids)
+{
+
+    for( let i = 0; i < arrayOfBoids.length;i++)
+    {
+        r1 = rule1(arrayOfBoids)
+        r2 = rule2(arrayOfBoids[i],arrayOfBoids)
+        r3 = rule3(arrayOfBoids[i],arrayOfBoids)
+        // console.log(C)
+        if(arrayOfBoids[i].moves >= (50 + arrayOfBoids[i].stamina))
+        {
+            arrayOfBoids[i].live()
+            arrayOfBoids[i].moves = 0
+        }
+        arrayOfBoids[i].applyForce(createVector(randomMovement(),randomMovement()),r1,r2,r3) //????
+        arrayOfBoids[i].update()
+        arrayOfBoids[i].display()
+    }
+}
+
+//get the centter of mass
+function rule1(arrayOfBoids)
+{
+    c = createVector(0,0)
+
+    for(i =0; i < arrayOfBoids.length;i++)
+    {
+        c.add(arrayOfBoids[i].position)
+    }
+
+}
+
+
+//check the distance between each boid
+function rule2(boid,boidArray)
+{
+    c = createVector(0,0)
+
+    for(i = 0; i<boidArray.length;i++)
+    {
+        if(boid.idNumber != boidArray[i].idNumber)
+        {
+            if(abs(boid.position.dist(boidArray[i].position)) < 100)
+            {
+                c = p5.Vector.sub(c,(p5.Vector.sub(boid.position,boidArray[i].position))) 
+            }
+        }
+    }
+    return c
+}
+
+function rule3(boid,boidArray)
+{
+    c = createVector(0,0)
+
+    for(i = 0; i < boidArray.length;i++)
+    {
+        if(boid.idNumber != boidArray[i].idNumber)
+        {
+            c = c.add(boidArray[i].velocity)
+
+        }
+
+    }
+
+    c = c.div(boidArray.length-1)
+    c = c.sub(boid.velocity)
+    c = c.div(8) //why 8?
+    return c
+}
